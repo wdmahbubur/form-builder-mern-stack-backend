ddbe4fcdafc85757f7865bb083271a5b8d236406
Mahbubur Rahman <mrrohanc@gmail.com>
2022-01-27 04:23:28 +0600

    mongodb cluster change

--- config/keys.js
+++ config/keys.js
@@ -5,5 +5,5 @@ const pass = process.env.MONGODB_PASSWORD;
 
 module.exports = {
   mongoURIDev: "mongodb://localhost:27017/form-builder-mern-stack",
-  mongoURI: `mongodb+srv://${user}:${pass}@cluster0.ni4ot.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`, //Cluster uri
+  mongoURI: `mongodb+srv://${user}:${pass}@cluster0.ni4ot.mongodb.net/form-builder-mern-stack?retryWrites=true&w=majority`, //Cluster uri
 };
